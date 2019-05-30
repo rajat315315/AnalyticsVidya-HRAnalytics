8cd81f1b6c728afaa67d09af90d48d5d1e45bc4f
Rajat Jain <rajat.jain@knowdisdata.com>
2019-05-30 14:28:58 +0530

    Running version updated

--- HRAnalytics.py
+++ HRAnalytics.py
@@ -6,22 +6,30 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
 
 train = pd.read_csv(os.path.join('Data', 'train.csv'))
 test = pd.read_csv(os.path.join('Data', 'test.csv'))
 
-is_promoted = train.drop('is_promoted')
-concat = pd.concat([train, test])
+is_promoted = train['is_promoted']
+train = train.drop(['is_promoted', ], 1)
+concat = pd.concat([train, test], ignore_index=True)
 
-concat['previous_year_rating'] = concat.groupby('KPIs_met >80%')['previous_year_rating'].fillna('mean')
-# concat.replace({'Bachelor\'s' : 1, 'Master\'s & above':2, 'Below Secondary': 0})
+# concat.replace({'Bachelor\'s' : 1.0, 'Master\'s & above':2.0, 'Below Secondary': 0.0}, inplace=True)
+concat['previous_year_rating'] = concat.groupby('KPIs_met >80%')['previous_year_rating'].apply(lambda x: x.fillna(x.mean()))
+# concat = concat.dropna(subset=['education'], axis=0)
 
+concat = pd.concat([concat, pd.get_dummies(concat['department'])], 1)
+concat = pd.concat([concat, pd.get_dummies(concat['region'])], 1)
+concat = pd.concat([concat, pd.get_dummies(concat['gender'])], 1)
+concat = pd.concat([concat, pd.get_dummies(concat['recruitment_channel'])], 1)
+concat = concat.drop(['education', 'department', 'recruitment_channel', 'gender', 'region', 'employee_id'], 1)
 
 models = [GradientBoostingClassifier(), RandomForestClassifier(), DecisionTreeClassifier(), ExtraTreeClassifier(), KNeighborsClassifier(), BernoulliNB()]
 
 
 params = [
-    {},
+    {'loss':['deviance', 'exponential'], 'n_estimators':[10, 50, 100],'criterion':['friedman_mse', 'mse', 'mse'], 'max_depth':[3, 10, 30, 50], 'min_impurity_decrease': [0.0, 0.01, 0.05, 0.1], 'max_features': ['auto', 'sqrt', 'log2', None]},
     {'n_estimators': [10, 50, 100], 'criterion':['entropy','gini'], 'max_depth': [None, 10, 20, 30], 'max_features': ['auto', 'sqrt', 'log2', None], 'min_impurity_decrease': [0.0, 0.01, 0.05, 0.1], 'bootstrap':[True, False], 'n_jobs':[-1]},
     {'criterion':['entropy','gini'], 'max_depth': [None, 10, 20, 30], 'max_features': ['auto', 'sqrt', 'log2', None], 'min_impurity_decrease': [0.0, 0.01, 0.05, 0.1]},
     {'criterion':['entropy','gini'], 'max_depth': [None, 10, 20, 30], 'max_features': ['auto', 'sqrt', 'log2', None], 'min_impurity_decrease': [0.0, 0.01, 0.05, 0.1]},
@@ -29,3 +37,13 @@ params = [
     {'alpha': [0.2, 0.5, 1.0], 'fit_prior':[True, False]},
 ]
 
+print(concat)
+for model, param in zip(models, params):
+
+
+    gcv = GridSearchCV(estimator=model, param_grid=param, n_jobs=-1, cv=5, scoring='f1')
+    gcv.fit( concat.iloc[:train.shape[0], :] , is_promoted)
+
+    # gcv.best_estimator_
+    print(gcv.best_params_)
+    print(gcv.best_score_)
